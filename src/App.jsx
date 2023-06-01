03d38f9dbb69e294d24a42c45ec033dc8e83f43f
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-05-31 21:05:38 -0300

    agrego estructura basica

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Saludo from './componentes/Saludo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+     <Saludo nuevoSaludo='Hello World!'></Saludo>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Saludo.jsx
@@ -0,0 +1,9 @@
+const Saludo = ({nuevoSaludo}) => {
+    return (
+        <div>
+            <h1>{nuevoSaludo}</h1>
+        </div>
+    );
+};
+
+export default Saludo;
